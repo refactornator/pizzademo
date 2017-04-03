492f14bd623c460626621611dc178075b6ae42b2
William Lindner <wlindner522@gmail.com>
2017-04-03 15:30:23 -0700

    Add an order_pizza method

--- lib/pizzaservice.ex
+++ lib/pizzaservice.ex
@@ -5,4 +5,11 @@ defmodule Pizzaservice do
 			"Pepperoni" => 10
 		}
 	end
+
+	def order_pizza(name) do
+		case Map.get(menu, name) do
+			nil -> {:error, "No such pizza #{name}"}
+			price -> {:ok, %{price: price, msg: "Enjoy!"}}
+		end
+	end
 end
